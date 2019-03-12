74cb913bc8ec2c0f848b4c352c085f997555b976
Bhavna Thakur <x18145914@student.ncirl.ie>
2019-03-12 10:53:07 +0000

    1.0.1

---
+++ db/migrate/20190312101706_create_orders.rb
@@ -0,0 +1,11 @@
+class CreateOrders < ActiveRecord::Migration[5.2]
+  def change
+    create_table :orders do |t|
+      t.string :firstname
+      t.string :lastname
+      t.decimal :sub_total
+
+      t.timestamps
+    end
+  end
+end
